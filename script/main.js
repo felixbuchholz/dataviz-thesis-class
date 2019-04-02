dad86150624a48493e70d4765d0dada9db52ce9b
felixbuchholz <felixbuchholz@gmx.net>
2019-04-02 13:06:05 -0400

    hello world example done

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
-d3.csv(./data/cps_00004.csv).then(function(data) {
+d3.csv('/data/cps_00004.csv').then(function(data) {
+  const firstEntry = data[0]
+  d3.select('#hidata').text(`${firstEntry.YEAR}`)
   console.log(data[0]);
 })
