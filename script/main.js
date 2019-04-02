47c47c1cea7ecc81e9b597b4362defb589a242ef
felixbuchholz <felixbuchholz@gmx.net>
2019-04-02 13:21:37 -0400

    correct link

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-d3.csv('/data/cps_00004.csv').then(function(data) {
+d3.csv('data/cps_00004.csv').then(function(data) {
   const firstEntry = data[0]
   d3.select('#hidata').text(`${firstEntry.YEAR}`)
   console.log(data[0]);
